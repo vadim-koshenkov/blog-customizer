f4ade35f16332241626b1e2d1960dcc00db0316e
Vadim Koshenkov <misterywou@yandex.ru>
2025-10-18 22:41:52 +0300

    ПР-10, реализована функциональность приложения

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,124 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Text } from 'src/ui/text'
+import { Select } from 'src/ui/select'
+import { RadioGroup } from 'src/ui/radio-group';
+import { fontFamilyOptions, fontSizeOptions, fontColors, backgroundColors, contentWidthArr, defaultArticleState, OptionType, ArticleStateType } from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+import { useState, useRef, useEffect } from "react";
+import { Separator } from 'src/ui/separator';
+
+type ArticleParamsFormProps = {
+	formState: boolean;
+	toggleFunction: (option?: boolean) => void;
+	setSettingsFunction: (settings: ArticleStateType) => void;
+}
+
+export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
+	const [formSettings, setFormSettings] = useState<ArticleStateType>(defaultArticleState);
+	const sidebarRef = useRef<HTMLElement>(null);
+
+	useEffect(() => {
+		document.addEventListener('mousedown', clickOutside);
+
+		return () => {
+			document.removeEventListener('mousedown', clickOutside);
+		};
+	}, [props.formState, props.toggleFunction]);
+
+	const handleFormChange = (selected: OptionType, optionName: keyof ArticleStateType) => {
+		setFormSettings(prev => ({
+			...prev,
+			[optionName]: selected
+		}));
+	};
+
+	const clickOutside = (e: MouseEvent) => {
+			if (props.formState && sidebarRef.current && !sidebarRef.current.contains(e.target as Node)) {
+				props.toggleFunction(false);
+			}
+		};
+	
+	const resetFormSettings = () => {
+		setFormSettings(defaultArticleState);
+		props.setSettingsFunction(defaultArticleState);
+		props.toggleFunction(false);
+	};
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		props.setSettingsFunction(formSettings);
+		props.toggleFunction(false);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton 
+				isOpen={props.formState} 
+				onClick={() => props.toggleFunction()} 
+			/>
+			<aside 
+				ref={sidebarRef} 
+				className={clsx(styles.container, { 
+					[styles.container_open]: props.formState 
+				})}
+			>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<Text 
+					as={'h2'} 
+					size={31} 
+					weight={800} 
+					uppercase
+				>
+					Задайте параметры
+				</Text>
+					<Select 
+						selected={formSettings.fontFamilyOption} 
+						onChange={(selected) => handleFormChange(selected, 'fontFamilyOption')} 
+						options={fontFamilyOptions} 
+						title='шрифт'
+					/>
+					<RadioGroup 
+						selected={formSettings.fontSizeOption} 
+						name='font-size' 
+						onChange={(selected) => handleFormChange(selected, 'fontSizeOption')} 
+						options={fontSizeOptions} 
+						title='размер шрифта'
+					/>
+					<Select 
+						selected={formSettings.fontColor} 
+						onChange={(selected) => handleFormChange(selected, 'fontColor')} 
+						options={fontColors} 
+						title='цвет шрифта'
+					/>
+					<Separator/>
+					<Select 
+						selected={formSettings.backgroundColor} 
+						onChange={(selected) => handleFormChange(selected, 'backgroundColor')} 
+						options={backgroundColors} 
+						title='цвет фона'
+					/>
+					<Select 
+						selected={formSettings.contentWidth} 
+						onChange={(selected) => handleFormChange(selected, 'contentWidth')} 
+						options={contentWidthArr} 
+						title='ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button 
+							title='Сбросить' 
+							htmlType='button' 
+							type='clear'
+							onClick={resetFormSettings}
+						/>
+						<Button 
+							title='Применить' 
+							htmlType='submit' 
+							type='apply'
+						/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -4,28 +4,45 @@ import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { ArticleStateType, defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
+
+import { useState, useCallback } from "react";
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [pageSettings, setPageSettings] = useState<ArticleStateType>(defaultArticleState);
+
+	const handlePageSettings = (newSettings: ArticleStateType) => {
+		setPageSettings(newSettings);
+	}
+
+	const toggleSidebar = (option?: boolean) => {
+		option ? setIsOpen(option) : setIsOpen(!isOpen)
+	}
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': pageSettings.fontFamilyOption.value,
+					'--font-size': pageSettings.fontSizeOption.value,
+					'--font-color': pageSettings.fontColor.value,
+					'--container-width': pageSettings.contentWidth.value,
+					'--bg-color': pageSettings.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm 
+				formState={isOpen} 
+				toggleFunction={toggleSidebar}
+				setSettingsFunction={handlePageSettings}
+			/>
 			<Article />
 		</main>
 	);
